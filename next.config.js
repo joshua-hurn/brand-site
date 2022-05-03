b68c2eec806e238c405e92576b5af0e9ce289cd9
Josh Hurn <joshua.hurn@hotmail.com>
2022-05-02 21:14:09 -0500

    build id in next config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     env: {
         FONTAWESOME_NPM_AUTH_TOKEN: process.env.FONTAWESOME_NPM_AUTH_TOKEN
     },
+    generateBuildId: async () => {
+        let id;
+        
+        require('child_process').exec('git rev-parse HEAD', function (err, stdout) {
+            id = stdout
+        });
+        
+        return id
+    },
 };
