0c75c121508a9def015080cb24b5099f9a25dc91
Josh Hurn <joshua.hurn@hotmail.com>
2022-05-06 10:03:15 -0500

    fontawesome is trash

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import styles from "../styles/modules/header.module.scss";
+// import Image from "next/image";
+// import styles from "../styles/modules/header.module.scss";
 
 const Header = () => {
   const [links] = useState([
@@ -12,9 +12,9 @@ const Header = () => {
   return (
     <div className="header-wrapper d-flex flex-column align-items-center">
       <h1 className="title d-flex align-items-center">
-        <Image src="/assets/icons/bracket-curly-left.svg" width={30} height={60} className={styles.filterCurly}/>
+        {/* <Image src="/assets/icons/bracket-curly-left.svg" width={30} height={60} className={styles.filterCurly}/> */}
         <span className="m-3">Josh Hurn</span>
-        <Image src="/assets/icons/bracket-curly-right.svg" width={30} height={60} className={styles.filterCurly}/>
+        {/* <Image src="/assets/icons/bracket-curly-right.svg" width={30} height={60} className={styles.filterCurly}/> */}
       </h1>
 
       {links.map((link, index) => (
