8dbb45514993fdcc8075aa250e3a799431b3d566
Josh Hurn <joshua.hurn@hotmail.com>
2022-05-06 10:17:25 -0500

    fontawesome not the problem

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faHome } from '@fortawesome/pro-solid-svg-icons';
-// import '@fortawesome/fontawesome-svg-core/styles.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome } from '@fortawesome/pro-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.scss';
 
-// library.add(faHome);
+library.add(faHome);
 
 function Application({ Component, pageProps }) {
   useEffect(() => {
