6d1368123a63d6b879bdcc29179dfb3f2be2c7e6
Josh Hurn <joshua.hurn@hotmail.com>
2022-05-08 08:38:16 -0500

    footer logos mostly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import styles from '../styles/modules/footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      hurn.io
+      <Link href={"https://github.com/joshua-hurn"}>
+        <a className={styles.logoLink}>
+          <img src={"/assets/images/github-square.svg"} width={30} height={60} />
+        </a>
+      </Link>
+
+      <Link href={"https://www.linkedin.com/in/josh-hurn/"}>
+        <a className={styles.logoLink}>
+          <img src={"/assets/images/linkedin.svg"} width={30} height={60} />
+        </a>
+      </Link>
     </footer>
   )
 }
