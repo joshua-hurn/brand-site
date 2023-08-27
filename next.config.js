61772158cfad4a335c066a2f7b0d0dddbaebd43d
Josh Hurn <joshua.hurn@hotmail.com>
2023-08-26 22:53:59 -0500

    remove the rest of Font Awesome & remove id project URL for now

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const fs = require("fs");
 
 module.exports = {
-    env: {
-        FONTAWESOME_NPM_AUTH_TOKEN: process.env.FONTAWESOME_NPM_AUTH_TOKEN
-    },
     generateBuildId: async () => {
         const rev = fs.readFileSync('.git/HEAD').toString().trim();
         if (rev.indexOf(':') === -1) {
